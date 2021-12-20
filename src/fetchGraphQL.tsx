f4ca0a9fa6ea10339ab6bde77babf2f0a73c6668
payalkherajani <payal.kherajani19@gmail.com>
2021-12-20 21:33:50 +0530

    without-relay-fetch-graphql-data

---
+++ src/fetchGraphQL.tsx
@@ -0,0 +1,22 @@
+
+async function fetchGraphQL(text: any, variables?: any) {
+  const REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN = process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN;
+
+  // Fetch data from GitHub's GraphQL API:
+  const response = await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `bearer ${REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: text,
+      variables,
+    }),
+  });
+
+  // Get the response as JSON
+  return await response.json();
+}
+
+export default fetchGraphQL;
